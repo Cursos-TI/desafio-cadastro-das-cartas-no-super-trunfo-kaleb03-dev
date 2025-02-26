62a89136cb8fa1f87341ae4df9a6b77a3cf2a7ad
kaleb03-dev <kalebjosias17@gmail.com>
2025-02-26 16:15:08 -0300

    iniciei o projeto SuperTrunfo

--- Desafio Super Trunfo .c
+++ Desafio Super Trunfo .c
@@ -2,10 +2,22 @@
 
 int main(){
     printf("Desafio Super Trunfo");
+    printf("Tema: Países.");
 
+    char estado;
+    char codigodecarta[20];
+    char nomedacidade[20];
+    int populacao;
+    int numerodepontosturisticos;
+    float area;
+    float pib;
 
-
-
+    //Carta 1:
+    printf("Carta 1: \n");
+    //Uma letra de 'A'a'H'(representando um dos oitos estados).
+    printf("Estado: \n");
+    scanf("%c", &estado);
+    
 
 
 }
