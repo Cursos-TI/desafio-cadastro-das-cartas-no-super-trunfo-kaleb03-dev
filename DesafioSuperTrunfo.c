3a2577da724d6bb2113216b237f755dcf6876e0b
kaleb03-dev <kalebjosias17@gmail.com>
2025-03-31 15:49:46 +0000

    Inicio do super trunfo

--- DesafioSuperTrunfo.c
+++ DesafioSuperTrunfo.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main(){
-    printf("Desafio Super Trunfo\n"); 
-} 
+
+printf("Carta 1:\n");
+char estado;
+char codigo[20];
+char nomedacidade[20];
+int populacao;
+float area;
+float pib;
+int pontosturisticos;
+
+printf("Digite o Estado:\n");
+scanf("%c", &estado);
+printf("%c", estado);
+
+
+
+
+
+
+
+}
